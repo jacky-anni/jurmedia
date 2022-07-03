64e436091233994e34b19139c1e5480a1552875a
jacky-anni <54279690+jacky-anni@users.noreply.github.com>
2022-07-03 08:42:46 -0700

    version a heberger

--- admin/class/Mail.php
+++ admin/class/Mail.php
@@ -18,15 +18,15 @@ class Mail {
     //Server settings
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host = 'formations.jurimedia.org';                     //Set the SMTP server to send through
+            $mail->Host = 'mail.jurimedia.org';                     //Set the SMTP server to send through
             $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-            $mail->Username = 'team@formations.jurimedia.org';                     //SMTP username
+            $mail->Username = 'team@jurimedia.org';                     //SMTP username
             $mail->Password = 'jurimedia2022';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-            $mail->setFrom('team@formations.jurimedia.org', 'Equipe de jurimedia');
+            $mail->setFrom('team@jurimedia.org', 'Equipe de jurimedia');
             $mail->addAddress($user->email, $user->prenom);     //Add a recipient
 
     //Content
